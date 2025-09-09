cb5e510ca1eff880e7a7c6e3d68bf3cb0617f9cd
Pablo <inzunza.0100@gmail.com>
2025-09-09 18:11:03 -0300

    Modificación 1

--- 0_proyectopdf_4/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace proyectopdf.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
